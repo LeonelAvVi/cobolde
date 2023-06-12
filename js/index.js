3c6725068e0fa9e36d86af4485dae2bea35f5d01
Leonel <leonel.sis12@gmail.com>
2023-06-12 12:16:52 -0400

    Loading and Fixed

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ function enviarWhatsapp(event) {
     var mensaje = document.getElementById('myForm').elements['contact-message'].value;
 
     // Construir la URL de WhatsApp con los datos prellenados
-    var whatsappURL = 'https://api.whatsapp.com/send?phone=59175400011&text=Hola,%20soy%20' + encodeURIComponent(name) + '.%20Vengo%20de%20dela%20pagina%20web%20%0AMi%20mensaje%20es:%20' + encodeURIComponent(mensaje);
+    var whatsappURL = 'https://api.whatsapp.com/send?phone=59171164644&text=Hola,%20soy%20' + encodeURIComponent(name) + '.%20Vengo%20de%20dela%20pagina%20web%20%0AMi%20mensaje%20es:%20' + encodeURIComponent(mensaje);
 
     // Redirigir al usuario a la página de WhatsApp con los datos prellenados
     window.location.href = whatsappURL;
